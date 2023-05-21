addd13eb00ee64695694b900a05a655d8d5f7069
868jjm <77506107+868jjm@users.noreply.github.com>
2023-05-21 01:53:58 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -260,7 +260,7 @@
         function onWindowResize( e ) {
             containerWidth = container.clientWidth;
             containerHeight = container.clientHeight;
-            renderer.setSize( containerWidth, containerHeight );
+            renderer.setSize( containerWidth, containerHeight,false );
             camera.aspect = containerWidth / containerHeight;
             camera.updateProjectionMatrix();
         }
