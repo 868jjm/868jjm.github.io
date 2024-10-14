70c21acc29cee6b7354faf23e9a9e7faa62770d8
868jjm <juergenjeanmarie@gmail.com>
2024-10-15 00:22:00 +0200

    002115102024

--- script.js
+++ script.js
@@ -56,7 +56,9 @@ let text1 = new THREE.Group();
         new THREE.Vector2(metrics.actualBoundingBoxLeft - padding, -metrics.fontBoundingBoxDescent - padding),
         new THREE.Vector2(metrics.actualBoundingBoxRight + padding, metrics.fontBoundingBoxAscent + padding)
     ).getSize(text_size);
-    text_size.multiplyScalar(goldenRatio);
+    // Multiply the existing values by the golden ratio
+    text_size.x *= goldenRatio;
+    text_size.y *= goldenRatio;
     canvas.width = Math.ceil(text_size.x);
     canvas.height = Math.ceil(text_size.y);
     // draw the texture on canvas
